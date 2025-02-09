ea5ca44d5eb951cf479233bff99895cfaace67b5
VladKvaskov <vladkvaskov@yahoo.com>
2025-02-10 00:09:26 +0200

    pass base_url to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
 
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const base = process.env.BASE_URL || "/"
+
+console.log('BASE',base);
 // https://vite.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
-})
+});
