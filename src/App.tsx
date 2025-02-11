f987e33b25273f23855cc0273f7d33935269a937
VladKvaskov <vladkvaskov@yahoo.com>
2025-02-11 11:02:24 +0200

    [minor] Themes - Dark & Light

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import './App.css'
-import Calculator from './Calculator';
+import { useMemo, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import { IconButton, type PaletteMode } from "@mui/material";
 
-function App() {
+import "./App.css";
+import Calculator from "./Calculator";
+
+const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const savedMode = localStorage.getItem("mode");
+  const [mode, setMode] = useState<PaletteMode>(
+    savedMode ? (savedMode as PaletteMode) : prefersDarkMode ? "dark" : "light"
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode,
+        },
+      }),
+    [mode]
+  );
+
+  const toggleMode = () => {
+    setMode((prevMode) => {
+      const newMode = prevMode === "light" ? "dark" : "light";
+      localStorage.setItem("mode", newMode);
+
+      return newMode;
+    });
+  };
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <IconButton
+        onClick={toggleMode}
+        sx={{ position: "fixed", top: 0, right: 0 }}
+      >
+        {mode === "light" ? <NightsStayIcon /> : <LightModeIcon />}
+      </IconButton>
+      <CssBaseline />
       <Calculator />
-    </div>
+    </ThemeProvider>
   );
-}
+};
 
-export default App
+export default App;
